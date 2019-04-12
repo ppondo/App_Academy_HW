e9cc7b6f41fe07bc2a61f122173b0f5c735fef71
ppondo <patrickpondo7@gmail.com>
2019-04-11 21:04:13 -0400

    create hw files

---
+++ W1D5/stack.rb
@@ -0,0 +1,19 @@
+class Stack
+    attr_accessor :ivar
+    def initialize(ivar)
+      @ivar = ivar
+    end
+
+    def push(el)
+      @ivar << el
+    end
+
+    def pop
+      @ivar.pop
+    end
+
+    def peek
+      return @ivar[-1]
+    end
+
+end
