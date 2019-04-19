1db9b4672e243525b88a8cbd1a5f9c1f20f18cd8
ppondo <patrickpondo7@gmail.com>
2019-04-18 20:00:04 -0400

    Complete W2D5 HW

---
+++ W2D5_HW/lru_cache.rb
@@ -0,0 +1,33 @@
+class LRUCache
+    attr_accessor :size, :cache
+
+    def initialize(size)
+        @size = size
+        @cache = Array.new
+    end
+
+    def count
+      cache.count
+    end
+
+    def add(el)
+        if cache.include?(el)
+            index = cache.index(el)
+            cache.delete_at(index)
+            cache << el
+        elsif self.count != size
+            cache << el 
+        elsif !cache.include?(el)
+            cache.delete_at(0)
+            cache << el
+        end
+    end
+
+    def show
+      cache
+    end
+
+    private
+    # helper methods go here!
+
+  end
