b43e5b113cd9e5558e9d6281d76af78fbdcc3553
ppondo <patrickpondo7@gmail.com>
2019-04-22 20:57:20 -0400

    Finish W3D2 HW

--- W3D2_HW/plays.rb
+++ W3D2_HW/plays.rb
@@ -48,4 +48,75 @@ class Play
         id = ?
     SQL
   end
+
+  def self.find_by_title(title)
+    PlayDBConnection.instance.execute(<<-SQL, self.title)
+      SELECT
+        title
+      FROM
+        plays
+      WHERE
+        title = title
+    SQL
+  end
+
+  def self.find_by_playwright(name)
+    PlayDBConnection.instance.execute(<<-SQL, self.playwright_id)
+      SELECT
+        playwright_id
+      FROM
+        plays
+      WHERE
+        playwright_id = name
+    SQL
+  end
+end
+
+class Playwright
+  def self.all
+   PlayDBConnection.instance.execute("SELECT playwright_id FROM plays")
+  end
+
+  def self.find_by_name(name)
+    Play.find_by_playwright(name)
+  end
+
+  def initialize
+    @id = id
+    @playwright_id = playwright_id
+  end
+
+  def create
+    raise "#{self} already in database" if self.playwright_id
+    PlayDBConnection.instance.execute(<<-SQL, self.id,  self.playwright_id)
+      INSERT INTO
+        play (id, playwright_id)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+     raise "#{self} not in database" unless self.playwright_id
+    PlayDBConnection.instance.execute(<<-SQL, self.playwright_id, self.id)
+      UPDATE
+        plays.playwright_id
+      SET
+        playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    PlayDBConnection.instance.execute(<<-SQL, self.playwright_id)
+      SELECT
+        title
+      FROM
+        plays
+      WHERE
+        playwright_id = ?;
+    SQL
+  end
 end
